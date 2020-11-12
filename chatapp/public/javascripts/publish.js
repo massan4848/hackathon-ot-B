61f1cf9bc639e467c3bb2103f6c7ce1d52dca54e
massan4848 <mfujii0823@gmail.com>
2020-11-13 00:32:21 +0900

    投稿日時04

--- chatapp/public/javascripts/publish.js
+++ chatapp/public/javascripts/publish.js
@@ -1,9 +1,8 @@
 'use strict';
-
 // 投稿メッセージをサーバに送信する
 function publish() {
     // ユーザ名を取得
-    const userName = $('#userName').text();
+    const userName = $('#userName').val();
     // 入力されたメッセージを取得
     const message = $('#message').val();
     // 投稿内容を送信
@@ -15,9 +14,7 @@ function publish() {
 
 // サーバから受信した投稿メッセージを画面上に表示する
 socket.on('receiveMessageEvent', function (data) {
-    let now = new Date();
-    $('#thread').prepend('<p>'+data.userName + 'さん：'+ data.message + '</p>');
-    $('#thread').prepend('<p><font size = "1">'+ toFormat('H:MI') + '</p>');
+    $('#thread').prepend('<p>' + data.userName + 'さん：'+ data.message +'<font size = "1">'+ " " + data.now + '</font>'+'</p>');
     // 投稿した後に投稿文を空にする
     $('#message').val('');
 });
--- chatapp/sockets/publish.js
+++ chatapp/sockets/publish.js
@@ -1,8 +1,12 @@
 'use strict';
+require("date-utils");
 
 module.exports = function (socket, io) {
     // 投稿メッセージを送信する
     socket.on('sendMessageEvent', function (data) {
-        io.sockets.emit('receiveMessageEvent',data)
+        var now = new Date();
+        data.now = now.toFormat('H:MI');
+        console.log(data);
+        io.sockets.emit('receiveMessageEvent',data);
     });
 };
