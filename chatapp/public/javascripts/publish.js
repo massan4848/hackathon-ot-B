43c6503b64ce6fe3dbbd3edc3b5630a75fea65f6
ashitani0803 <ashitani0803@gmail.com>
2020-11-12 20:06:21 +0900

    投稿者の名前を表示

--- chatapp/public/javascripts/publish.js
+++ chatapp/public/javascripts/publish.js
@@ -7,17 +7,17 @@ function publish() {
     // 入力されたメッセージを取得
     const message = $('#message').val();
     // 投稿内容を送信
-    if ( message.trim() !== '' ){
-        socket.emit('sendMessageEvent',{message,userName});
+    if (message.trim() !== '') {
+        socket.emit('sendMessageEvent', { message, userName });
     }
     return false;
 }
 
 // サーバから受信した投稿メッセージを画面上に表示する
 socket.on('receiveMessageEvent', function (data) {
+    const userName = $("#userName").val()
     let now = new Date();
-    $('#thread').prepend('<p>'+data.userName + 'さん：'+ data.message + '</p>');
-    $('#thread').prepend('<p>'+ +  + '</p>');
+    $('#thread').prepend('<p>' + userName + 'さん：' + data.message + '</p>');
     // 投稿した後に投稿文を空にする
     $('#message').val('');
 });
